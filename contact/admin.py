e1abeef7b4dd31314e1008eaf4dd594b8d97ba52
Rafael Henrique <rafaelh9six@gmail.com>
2023-08-01 14:21:48 -0300

    show the show field as a editable field

--- contact/admin.py
+++ contact/admin.py
@@ -6,7 +6,8 @@ from contact import models
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 
                     'last_name', 'phone', 
-                    'email', 'created_date', )
+                    'email', 'created_date', 
+                    'show', )
     list_display_links = ('id', 'first_name', 
                     'last_name', )
     list_filter = ('created_date', )
@@ -15,6 +16,7 @@ class ContactAdmin(admin.ModelAdmin):
                     'email', )
     ordering = ('id', )
     list_per_page = 10
+    list_editable = ('show', )
     
     
 
