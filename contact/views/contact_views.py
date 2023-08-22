8c884c70e563cfb4032b953b46fc4e0dd1b63c97
Rafael Henrique <rafaelh9six@gmail.com>
2023-08-22 14:54:39 -0300

    created dynamic title

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
             .order_by('-id')[:10]
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - '
     }
     return render(
         request,
@@ -23,9 +24,11 @@ def contact(request, contact_id):
         pk=contact_id,
         show=True
     )
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
     
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
     return render(
         request,
