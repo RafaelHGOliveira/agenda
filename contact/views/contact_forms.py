31d6669155d69185f32bba24366ecc8cf9cbe89c
Rafael Henrique <rafaelh9six@gmail.com>
2023-08-30 15:32:41 -0300

    removed some imports

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.db.models import Q
-from django.http import Http404
+from django.shortcuts import render
 
 from contact.models import Contact
 from contact.forms import ContactForm
