dc18669930e1e99bb2bf4a5ef86bd41032950746
Rafael Henrique <rafaelh9six@gmail.com>
2023-08-31 14:31:18 -0300

    deleteing contacts with confirmation

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -72,3 +72,28 @@ def update(request, contact_id):
         'contact/create.html',
         context,
     )
+    
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, 
+        pk=contact_id, 
+        show=True
+    )
+    
+    confirmation = request.POST.get('confirmation', 'no')
+    
+    context = {
+        'contact': contact,
+        'confirmation': confirmation,
+    }
+    
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+    
+    
+    return render(
+        request,
+        'contact/contact.html',
+        context,
+    )
