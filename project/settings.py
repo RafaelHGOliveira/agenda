8b2c51ce3392cb0405cb3b04315f2cc9259dc9d5
Rafael Henrique <rafaelh9six@gmail.com>
2023-09-05 15:29:09 -0300

    ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y6pl^kib3u*z6+su5omwi*f$gf82c3%ymq$gudluo%v+ba=vnq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://agenda-rafaelh9six.up.railway.app', '']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
